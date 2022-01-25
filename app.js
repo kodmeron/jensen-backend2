4f54e8c07bf3380ef66eebbacb00740066ab79db
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-01-25 16:03:24 +0100

    login formulär

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const credentials = {secretUser:"username" , secretPassword:"password"}
 const cors = require("cors")
 const express = require('express')
-    const app = express()
+const app = express()
+
 
 process.env.PORT = 3000
 
 app.use(cors())
 app.get("/", (req, res)=>{
-    res.set('WWW-Authenticate', 'Basic realm="Access to Index')
-    res.status(401).send("Unauthorized access")
+    const encodedAuth = (req.headers.authorization || '')
+        .split(' ')[1] || ''
+
+    const [name, password] = Buffer.from(encodedAuth, 'base64')
+    .toString().split(':')
+    if (user===credentials.secretUser && password===credentials.secretPassword){
+        res.status(200).send({"STATUS":"SUCCESS BRRRROOOORRRR"})
+    }else{
+        res.set('WWW-Authenticate', 'Basic realm="Access to Index')
+        res.status(401).send("Unauthorized access")
+    }
+
+    
 })
 
 app.listen(3000 , ()=>{
