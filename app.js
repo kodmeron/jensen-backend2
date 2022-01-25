a3a6afc7afaee29d25859046edc19a94b4f7820c
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-01-25 15:19:23 +0100

    CORS anrop tillåtna

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const cors = require("cors")
 const express = require('express')
     const app = express()
 
 process.env.PORT = 3000
 
+app.use(cors())
 app.get("/", (req, res)=>{
     res.set('WWW-Authenticate', 'Basic realm="Access to Index')
     res.status(401).send("Unauthorized access")
